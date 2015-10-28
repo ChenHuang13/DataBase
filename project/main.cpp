c0521e467770efaf05f5700647a7986213ba6900
huangsy13 <huangsy13@gmail.com>
2015-10-28 15:01:09 +0800

    update

--- project/main.cpp
+++ project/main.cpp
@@ -15,44 +15,44 @@ int main() {
     RecordManager *recordManager = new RecordManager();
     recordManager->createSQL("my.db");
     recordManager->openSQL("my.db");
-    recordManager->createTable("Student", 10);
+    int record_lenth;
+    recordManager->createTable("Student", record_lenth);
 
     cout << "insert test" << endl; //插入0到9的数据
     for (int i = 0; i < 10; i++) {
         Record *record = new Record();
-        BufType data = new unsigned int[10];
-        memset(data, 0, 10);
+        BufType data = new unsigned int[record_lenth];
+        memset(data, 0, record_lenth);
         data[0] = (unsigned int) i;
-        record->setBuffer(data, 10);
-        record->weith = 10;
+        record->setBuffer(data, record_lenth);
+        record->weith = record_lenth;
         recordManager->insert(*record); //记录插入测试
     }
-//
+
     recordManager->query(*(new Query()), true); //记录查询测试，查询所有数据
-//
 
     cout << "update test" << endl;
     BufType a = new unsigned int[1];
     a[0] = 5;
-    Query *source = new Query(0, 1, a);
+    Query *source = new Query(0, 1, a); //构造需要被更改的数据
     BufType b = new unsigned int[1];
     b[0] = 13;
-    Query *target = new Query(0, 1, b);
+    Query *target = new Query(0, 1, b); //构造更改的目标
     recordManager->update(*source, *target); //记录更新测试，把id = 5 的数据更新为 13
-//
+
     recordManager->query(*(new Query()), true); //记录查询测试，查询所有
-//
+
     cout << "delete test" << endl;//删除id从 0 到 4的数据
     for (int i = 0; i < 5; i++) {
         Record *record = new Record();
-        BufType data = new unsigned int[10];
-        memset(data, 0, 10);
+        BufType data = new unsigned int[record_lenth];
+        memset(data, 0, record_lenth);
         data[0] = (unsigned int) i;
-        record->setBuffer(data, 10);
-        record->weith = 10;
+        record->setBuffer(data, record_lenth);
+        record->weith = record_lenth;
         recordManager->delete_record(*record); //记录删除测试
     }
-//
+
     recordManager->query(*(new Query()), true); //记录查询测试
 
     recordManager->close();//关闭数据库
