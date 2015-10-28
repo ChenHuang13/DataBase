ff475583d35efc2b900f1b34cad7dcc49b4455de
huangsy13 <huangsy13@gmail.com>
2015-10-28 11:05:45 +0800

    update

--- project/main.cpp
+++ project/main.cpp
@@ -1,22 +1,37 @@
+
 /*
  * testfilesystem.cpp
  *
  *  Created on: 2015年10月6日
  *      Author: lql
  */
+
 #include "./bufmanager/BufPageManager.h"
 #include "./recordmanager/recordManager.h"
 
 using namespace std;
 
 int main() {
-    FileManager* fm = new FileManager();
-    BufPageManager* bpm = new BufPageManager(fm);
-    fm->createFile("testfile.txt"); //新建文件
-    RecordManager *recordManager = new RecordManager(bpm);
-    recordManager->insert(*(new Record()));
-    recordManager->insert(*(new Record()));
-    recordManager->update(*(new Query), *(new Query));
+    RecordManager *recordManager = new RecordManager();
+    recordManager->createSQL("my.db");
+    recordManager->openSQL("my.db");
+    recordManager->createTable("Student", 50);
+
+//    for (int i =  0; i < 10; i++){
+//        recordManager->insert(*(new Record())); //记录插入测试
+//    }
+//
+//    recordManager->query(*(new Query())); //记录查询测试
+//
+//    recordManager->update(*(new Query), *(new Query)); //记录更新测试
+//
+//    recordManager->query(*(new Query())); //记录查询测试
+//
+//    recordManager->delete_record(*(new Record())); //记录删除测试
+//
+//    recordManager->query(*(new Query())); //记录查询测试
+
+    recordManager->close();//关闭数据库
 
     return 0;
 }
--- project/recordmanager/recordManager.h
+++ project/recordmanager/recordManager.h
@@ -9,53 +9,66 @@
 #include "query.h"
 #include "vector"
 #include "../bufmanager/BufPageManager.h"
+#include "cstring"
+#include "cmath"
 
+//记录管理模块
 class RecordManager {
 private:
-    BufPageManager *bufPageManager;
-    FileManager *fileManager;
-	int SQLID;
+    BufPageManager *bufPageManager;  //传入的缓存管理模块
+    FileManager *fileManager;  //传入的文件管理模块
+    int SQLID; //打开文件的id
 public:
+    //构造函数
     RecordManager() {
     	fileManager = new FileManager();
-	    BufPageManager *bufPageManager = new BufPageManager(fm);
+        bufPageManager = new BufPageManager(fileManager);
     }
 
-    RecordManager(BufPageManager *bufPageManager) {
-		this.bufPageManager = bufPageManager;
+    //构造函数需要传入文件管理类和缓存管理类
+    RecordManager(FileManager *fileManager, BufPageManager *bufPageManager) {
+        this->fileManager = fileManager;
+        this->bufPageManager = bufPageManager;
 	}
 
+    //解析函数
     virtual ~RecordManager() {
 
     }
 
+    //插入记录函数
     int insert(Record record) {
 
     }
 
+    //删除记录函数
     int delete_record(Record record) {
 
     }
 
+    //查询函数，输入参数为查询类，输出为查询到的记录列表
     vector <Record> query(Query query_source) {
         vector <Record> list;
         return list;
     }
 
+    //根据旧的记录产生新纪录
     Record form_record(Record old, Query target) {
         return Record();
     }
 
+    //跟新函数
     int update(Query query_source, Query target) {
         vector <Record> record_list;
         record_list = query(query_source);
         for (int i = 0; i < record_list.size(); i++) {
-            Record record_new = form_record(record_list[i]., target);
+            Record record_new = form_record(record_list[i], target);
             delete_record(record_list[i]);
             insert(record_new);
         }
     }
 
+    //新建数据库
     bool createSQL(const char *name) {
 		fileManager->createFile(name);
 	    int fileID;
@@ -67,25 +80,34 @@ public:
 		b[1] = 1;
 		b += 32;
         bufPageManager->markDirty(index);
-		fileManager->closeFile(name);
+        bufPageManager->close();
+        fileManager->closeFile(fileID);
 		return true;
     }
 
+    //打开存在的数据库
 	bool openSQL(const char* name) {
 		fileManager->openFile(name, SQLID);
 	}
 
+    //新建表
 	bool createTable(const char *name, int width) {
 		int index;
-		BufType b = bufPageManager->allocPage(SQLID, 0, index, false);
-		ByfType e = b + PAGE_INT_NUM - PAGE_BOTTOM;
-		int& pageNum = b[1];
-		b += PAGE_TOP;
+
+        BufType b = bufPageManager->getPage(SQLID, 0, index); //获取第一页
+
+        BufType e = b + PAGE_INT_NUM - PAGE_BOTTOM; //获取末尾
+
+        unsigned int *pageHead = b;
+        unsigned int &pageNum = b[1];
+
+        b += PAGE_TOP; //跳过信息头
+
 		while (b[0]) b += 4;
 		while (e[0]) e -= e[0];
 		e[0] = ceil(strlen(name) / 4) + 1;
 		memcpy(e - e[0] + 1, name, strlen(name));
-		b[0] = 1, b[1] = e, b[2] = pageNum++;
+        b[0] = 1, b[1] = e - pageHead, b[2] = pageNum++, b[3] = width;
 		int pageID = b[2];
 		bufPageManager->markDirty(index);
 		b = bufPageManager->allocPage(SQLID, pageID, index, false);
@@ -93,7 +115,10 @@ public:
 		bufPageManager->markDirty(index);
 	}
 
-
+    bool close() {
+        bufPageManager->close(); //关闭缓存类并写回
+        fileManager->closeFile(SQLID); //关闭数据库文件
+    }
 };
 
 #endif //DATABASE_RECORDMANAGER_H
